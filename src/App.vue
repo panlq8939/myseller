bcf92a893f33722028fdd892d070b815cfe26e04
panlq8939 <panlq8939@163.com>
2018-05-11 18:58:26 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script src="../build/webpack.dev.conf.js"></script>
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="tab">i am tab</div>
+    <div class="content">i am content</div>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header';
+export default {
+  comments:{
+    'Header': header
+  }
+};
+</script>
+
+<style>
+#app {
+
+}
+</style>
